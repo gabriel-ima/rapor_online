59337a375ba3b2651250d3b948b54a2146444a1a
gabriel-ima <imanuelagabriel05@gmail.com>
2025-06-12 22:57:50 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";      // Nama host, biasanya 'localhost'
+$user = "root";           // Username MySQL default di XAMPP adalah 'root'
+$password = "";           // Password MySQL default biasanya kosong di XAMPP
+$database = "rapor_online";   // Ganti dengan nama database kamu
+
+// Membuat koneksi
+$conn = mysqli_connect($host, $user, $password, $database);
+
+// Cek koneksi
+if (!$conn) {
+    die("Koneksi ke database gagal: " . mysqli_connect_error());
+}
+?>
