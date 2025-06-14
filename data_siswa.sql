97822d670fcc22d92179f864a4a8be060b2809b6
gabriel-ima <imanuelagabriel05@gmail.com>
2025-06-15 01:06:05 +0700

    Add files via upload

---
+++ data_siswa.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Jun 2025 pada 13.50
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rapor_online`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_siswa`
+--
+
+CREATE TABLE `data_siswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `data_siswa`
+--
+ALTER TABLE `data_siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `data_siswa`
+--
+ALTER TABLE `data_siswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+INSERT INTO data_siswa (id, nama) VALUES
+('18', 'Jonathan Prasetyo'),
+('19', 'Maria Angeline Siregar'),
+('20', 'Fajar Ramadhan Putra'),
+('21', 'Sherly Anindya Wijaya'),
+('22', 'Kevin Alvaro Nugroho'),
+('23', 'Nabila Zahra Maulida'),
+('24', 'Rizky Dwi Saputra');
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
