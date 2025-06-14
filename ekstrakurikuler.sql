9c93b647b3879188bb6f58a584b9f48abfc0f9b3
gabriel-ima <imanuelagabriel05@gmail.com>
2025-06-15 01:06:57 +0700

    Add files via upload

---
+++ ekstrakurikuler.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Jun 2025 pada 20.06
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rapor_online`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ekstrakurikuler`
+--
+
+CREATE TABLE `ekstrakurikuler` (
+  `id` int(11) NOT NULL,
+  `rapor_id` int(11) DEFAULT NULL,
+  `nama_kegiatan` varchar(100) DEFAULT NULL,
+  `keterangan` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `ekstrakurikuler`
+--
+
+INSERT INTO `ekstrakurikuler` (`id`, `rapor_id`, `nama_kegiatan`, `keterangan`) VALUES
+(18, NULL, 'Pramuka', 'bagus');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `ekstrakurikuler`
+--
+ALTER TABLE `ekstrakurikuler`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `rapor_id` (`rapor_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `ekstrakurikuler`
+--
+ALTER TABLE `ekstrakurikuler`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `ekstrakurikuler`
+--
+ALTER TABLE `ekstrakurikuler`
+  ADD CONSTRAINT `ekstrakurikuler_ibfk_1` FOREIGN KEY (`rapor_id`) REFERENCES `rapor` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
