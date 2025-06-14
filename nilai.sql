713651a7196d38571c118173143b1d8fc8cc07bf
gabriel-ima <imanuelagabriel05@gmail.com>
2025-06-15 01:04:27 +0700

    Update nilai.sql

--- nilai.sql
+++ nilai.sql
@@ -1,19 +1,85 @@
-CREATE TABLE nilai (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    siswa_id INT NOT NULL,
-    guru_id INT NOT NULL,
-    kelas VARCHAR(100),
-    kurikulum VARCHAR(100),
-    mapel VARCHAR(100),
-    nilai_intra INT,
-    -- nilai_ekstra INT,
-    rata_rata FLOAT,
-    predikat CHAR(1),
-    deskripsi TEXT,
-    -- sikap_spiritual TEXT,
-    -- sikap_sosial TEXT,
-    semester VARCHAR(10),
-    tahun_ajaran VARCHAR(20),
-    FOREIGN KEY (siswa_id) REFERENCES users(id),
-    FOREIGN KEY (guru_id) REFERENCES users(id)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Jun 2025 pada 20.03
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rapor_online`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nilai`
+--
+
+CREATE TABLE `nilai` (
+  `id` int(11) NOT NULL,
+  `siswa_id` int(11) NOT NULL,
+  `kelas` varchar(10) NOT NULL,
+  `kurikulum` varchar(50) NOT NULL,
+  `mapel` varchar(50) NOT NULL,
+  `nilai_intra` int(11) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `semester` enum('Ganjil','Genap') NOT NULL,
+  `tahun_ajaran` varchar(20) NOT NULL,
+  `guru_username` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `nilai`
+--
+
+INSERT INTO `nilai` (`id`, `siswa_id`, `kelas`, `kurikulum`, `mapel`, `nilai_intra`, `deskripsi`, `semester`, `tahun_ajaran`, `guru_username`, `created_at`) VALUES
+(1, 23, 'k4', 'KurikulumMerdeka', 'pai', 67, 'tingkatkan lagi', 'Ganjil', '2025', 'Hasanudin, S.Pd.I', '2025-06-14 17:58:15');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `nilai`
+--
+ALTER TABLE `nilai`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `siswa_id` (`siswa_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `nilai`
+--
+ALTER TABLE `nilai`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `nilai`
+--
+ALTER TABLE `nilai`
+  ADD CONSTRAINT `nilai_ibfk_1` FOREIGN KEY (`siswa_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
