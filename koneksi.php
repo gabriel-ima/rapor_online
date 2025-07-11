5a9c0a3124fcc0519b753737ea352cce6ab88999
gabriel-ima <imanuelagabriel05@gmail.com>
2025-07-11 11:50:36 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = "localhost";      // Nama host, biasanya 'localhost'
 $user = "root";           // Username MySQL default di XAMPP adalah 'root'
 $password = "";           // Password MySQL default biasanya kosong di XAMPP
-$database = "rapor_online";   // Ganti dengan nama database kamu
+$database = "rapor_online";   // Ganti dengan nama database kita
 
 // Membuat koneksi
 $conn = mysqli_connect($host, $user, $password, $database);
